819dd8b66d5574cd4adc1b542d248bbe3cc7f91b
Brewu <116376876+Brewu@users.noreply.github.com>
2026-02-21 05:02:36 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -25,8 +25,7 @@ connectDB();
 app.use(cors({
   origin: [
    "https://pulse-backend-tpg8.onrender.com",
-    "https://rabiesspulse.vercel.app",
-    "https://rabbiespulse-diyk12auu-brewu-richard-appiahs-projects.vercel.app"
+   "https://pulse-eta-cyan.vercel.app"
     // Add production origins here
   ],
   credentials: true,
